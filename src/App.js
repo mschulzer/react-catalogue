1e0cd6ef5c170ea3dbbc42c74bf8be718ca157c2
Morten Schultz <ms@brondby-gym.dk>
2024-07-05 17:33:28 +0200

    Added a Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { div } from "@tensorflow/tfjs";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       This is the starting code for "Your first component" ungraded lab
       <Header title="Hello, Morten" />
+      <Footer title="Goodbye!" />
     </div>
   );
 }
